e4ac878c17973a60d74cde9054c4fb91408a1302
lumoswitch <sofrona.anna@gmail.com>
2022-05-10 22:59:12 +0300

    changed lib name

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-  return src('lumos/**/*.scss')
+  return src('devcss/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['lumos/**/*.scss', '*.html'], buildStyles);
+  watch(['devcss/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
